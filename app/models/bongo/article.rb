c013dabf607d614970a6052117534e0a056bb71d
Thomas Hutterer <5372947+thutterer@users.noreply.github.com>
2020-06-04 16:22:38 +0200

    Share article partial on show and index

--- app/models/bongo/article.rb
+++ app/models/bongo/article.rb
@@ -7,5 +7,16 @@ module Bongo
     field :publish_at, type: Date
 
     scope :published, -> { where(:publish_at.lte => Date.today) }
+
+    # Mongoid (v7.0.8) doesn't have a cache_version method on its document.
+    # This causes https://jira.mongodb.org/browse/MONGOID-4680 when trying to
+    # use Rails collection caching.
+    def cache_version
+      if respond_to?(:updated_at)
+        updated_at.utc.to_s(:usec)
+      else
+        nil
+      end
+    end
   end
 end
